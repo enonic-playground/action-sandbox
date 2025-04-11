98fdd1f537cdf92d234db579a010be4092118098
Anatol Sialitski <asi@enonic.com>
2025-04-11 19:03:36 +0200

    save changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,8 @@ echo "Empty entrypoint"
 enonic --version
 
 enonic snapshot --help
+
+
+echo "XP_URL: ${ENONIC_CLI_REMOTE_URL}"
+echo "XP_USERNAME: ${XP_USERNAME}"
+echo "XP_PASSWORD: ${XP_PASSWORD}"
