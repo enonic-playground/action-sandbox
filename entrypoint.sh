4a45d17be224a957314f4580065002e13f7cd8f3
Anatol Sialitski <asi@enonic.com>
2025-04-14 10:15:07 +0200

    Test with:docker://

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,9 @@
 
 set -e
 
-echo "Empty entrypoint"
-
-enonic --version
-
-enonic snapshot --help
-
 echo "XP_URL: ${ENONIC_CLI_REMOTE_URL}"
 echo "XP_USERNAME: ${ENONIC_CLI_REMOTE_USER}"
 echo "XP_PASSWORD: ${ENONIC_CLI_REMOTE_PASS}"
+echo "CRED_FILE: ${ENONIC_CLI_CRED_FILE}"
+
+enonic --version
