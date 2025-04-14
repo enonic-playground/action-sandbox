68e9f31b31f7c5d86ea547e7ca578c0dcf33957d
Anatol Sialitski <asi@enonic.com>
2025-04-14 10:26:34 +0200

    Save changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ echo "XP_URL: ${ENONIC_CLI_REMOTE_URL}"
 echo "XP_USERNAME: ${ENONIC_CLI_REMOTE_USER}"
 echo "XP_PASSWORD: ${ENONIC_CLI_REMOTE_PASS}"
 echo "CRED_FILE: ${ENONIC_CLI_CRED_FILE}"
+echo "APP_JAR_FILE: ${APP_JAR}"
 
 enonic --version
+
+enonic app install --file ${APP_JAR}
